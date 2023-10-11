cdb49326ed184d4271bc2ca4890bb390c120d00f
samsonssali <ssali@jitpomi.com>
2023-10-10 22:00:32 -0600

    fix hypernews

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import AutoBase from 'autobase'
 import HyperSwarm from 'hyperswarm'
 import HyperBee from 'hyperbee'
 import crypto from 'crypto'
-//
 const args = minimist(process.argv.slice(2), {
     alias: {
         writers: 'w',
@@ -18,10 +17,12 @@ const args = minimist(process.argv.slice(2), {
     },
     boolean: ['ram', 'swarm']
 });
-
-
-//
-export default class HyperNews {
+function sha256(value) {
+    const hash = crypto.createHash('sha256');
+    hash.update(value);
+    return hash.digest('hex');
+}
+class HyperNews {
     constructor() {
         this.store = new CoreStore(args.ram ? ram : (args.storage || 'hypernews')) //
         this.swarm = null //
@@ -29,10 +30,9 @@ export default class HyperNews {
         this.bee = null
         this.name = null //
     }
-
     async start() {
         const writer = this.store.get({name: 'writer'})
-        const viewOutput = this.store.get({name: 'view'})
+        const viewOutput = this.store.get({name: 'view', valueEncoding: 'json'})
         await writer.ready()
         // read name from script or use the writer's key -- core.key
         this.name = args.name || writer.key.slice(0, 8).toString('hex')
@@ -42,31 +42,23 @@ export default class HyperNews {
             outputs: [viewOutput],
             localOutput: viewOutput,
         })
-
         // add more input cores to autobase via the node process
         for (const w of [].concat(args.writers || [])) {
             await this.autobase.addInput(this.store.get(Buffer.from(w, 'hex')))
         }
-
         // add more output cores to autobase via the node process
         for (const i of [].concat(args.indexes || [])) {
             await this.autobase.addOutput(this.store.get(Buffer.from(i, 'hex')))
         }
-
         await this.autobase.ready()
-
         if (args.swarm) {
-            const hash = crypto.createHash('sha256');
-            hash.update(this.name);
-            const hashedName = hash.digest('hex');
-            const topic = Buffer.from(hashedName, 'hex')
+            const topic = Buffer.from(sha256(this.name), 'hex')
             this.swarm = new HyperSwarm()
             this.swarm.on('connection', (socket) => this.store.replicate(socket))
             this.swarm.join(topic)
             await this.swarm.flush()
             process.once('SIGINT', () => this.swarm.destroy()) // for faster restarts
         }
-
         this.info();
         const self = this
         this.autobase.start({
@@ -75,45 +67,38 @@ export default class HyperNews {
                 const beeBatch = self.bee.batch({update: false})
                 for (const node of batch) {
                     const nodeValue = JSON.parse(node.value)
-                    let hash = crypto.createHash('sha256');
-                    hash.update(nodeValue.data);
-                    hash = hash.digest("hex")
-
+                    const hash = sha256(nodeValue.data);
                     if (nodeValue.type === 'post') {
-                        await beeBatch.put(`post${hash}`, {
+                        await beeBatch.put(`post!${hash}`, {
                             hash,
                             votes: 0,
                             data: nodeValue.data
                         })
                     }
-
                     if (nodeValue.type === 'vote') {
                         const increment = nodeValue?.up ? 1 : -1
-                        const theBee = await self.bee.get(`post${hash}`, {
+                        const blockEntryNode = await self.bee.get(`post!${hash}`, {
                             update: false
                         })
-                        if (theBee) {
-                            theBee.value.votes += increment
+                        if (blockEntryNode) {
+                            blockEntryNode.value.votes += increment
                         }
-                        await theBee.put(`post${hash}`, theBee.value)
-
+                        await beeBatch.put(`post!${hash}`, blockEntryNode.value)
                     }
                 }
+                await beeBatch.flush()
             }
         })
-
-        this.bee = new HyperBee(this.autobase.view, {
+        this.bee = new HyperBee(this.autobase.view.unwrap(), {
             extension: false,
             valueEncoding: "json",
             keyEncoding: 'utf-8'
         })
-
     }
-
     info() {
         console.log('Autobase setup. Pass this to run this same setup in another instance:')
         console.log()
-        console.log('node index.js ' +
+        console.log('hrepl index.js '+
             '-n ' + this.name + ' ' +
             this.autobase.inputs.map(i => '-w ' + i.key.toString('hex')).join(' ') + ' ' +
             this.autobase.outputs.map(i => '-i ' + i.key.toString('hex')).join(' ')
@@ -123,16 +108,41 @@ export default class HyperNews {
         console.log('To disable swarming add --no-swarm')
         console.log()
     }
-
-    async* all() {
-        for await (const data of this.bee.createReadStream({gt: 'posts!', lt: 'posts!~'})) {
+    // async all() {
+    //     const res= [];
+    //     for await (const data of this.bee.createReadStream({gt: 'post!', lt: 'post!~'})) {
+    //         res.push(data.value)
+    //     }
+    //     return res;
+    // }
+    async * all () {
+        for await (const data of this.bee.createReadStream({ gt: 'post!', lt: 'post!~' })) {
             yield data.value
         }
     }
+    async addPost(data) {
+        const hash = sha256(data)
+        this.autobase.append(JSON.stringify({
+            type: 'post',
+            hash,
+            data
+        }))
+    }
+    async upVote(hash) {
+        this.autobase.append(JSON.stringify({
+            type: 'vote',
+            hash,
+            up: true
+        }))
+    }
+    async downVote(hash) {
+        this.autobase.append(JSON.stringify({
+            type: 'vote',
+            hash,
+            up: false
+        }))
+    }
 }
-//
-const news = new HyperNews()
-
+export const news = new HyperNews()
 await news.start()
-//
 
